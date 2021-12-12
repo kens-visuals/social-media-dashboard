771326fdb808bbff882490b93f9128babb16c636
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2021-12-11 23:07:48 -0800

    add JS functionality

--- js/script.js
+++ js/script.js
@@ -1,25 +1,28 @@
-const btn = document.querySelector('button');
+const body = document.body;
 const checkbox = document.querySelector('.js-checkbox');
+const checkboxSpan = document.querySelector('.js-checkbox-span');
+const preferenceQuery = window.matchMedia('(prefers-color-scheme: dark)');
 
-checkbox.addEventListener('click', () => {
-  if (document.body.classList.contains('dark-mode')) {
-    document.body.classList.remove('dark-mode');
-    document.body.classList.add('light-mode');
-  } else {
-    document.body.classList.remove('light-mode');
-    document.body.classList.add('dark-mode');
-  }
-});
+const addDarkMode = () => {
+  checkboxSpan.classList.remove('is-checked');
+  checkbox.setAttribute('checked', true);
+  body.classList.remove('light-mode');
+  body.classList.add('dark-mode');
+};
 
-var preference_query = window.matchMedia('(prefers-color-scheme: dark)');
+const addLightMode = () => {
+  checkboxSpan.classList.add('is-checked');
+  checkbox.setAttribute('checked', false);
+  body.classList.remove('dark-mode');
+  body.classList.add('light-mode');
+};
 
-function checkPreference(query) {
-  if (query.matches) {
-    document.body.classList.add('dark-mode');
-  } else {
-    document.body.classList.remove('dark-mode');
-    document.body.classList.add('light-mode');
-  }
-}
+const toggleTheme = () =>
+  !body.classList.contains('dark-mode') ? addDarkMode() : addLightMode();
 
-window.addEventListener('DOMContentLoaded', checkPreference(preference_query));
+const checkPreference = () =>
+  preferenceQuery.matches && window.matchMedia ? addDarkMode() : addLightMode();
+
+checkbox.addEventListener('click', toggleTheme);
+preferenceQuery.addEventListener('change', checkPreference);
+window.addEventListener('DOMContentLoaded', checkPreference);
